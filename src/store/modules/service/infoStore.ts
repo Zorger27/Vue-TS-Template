612cf661de44f58c6db386d3d82a5f1c8de61a83
Anatolii Zorin <Zorger27@GMail.com>
2024-12-17 02:14:00 +0200

    Small changes

--- src/store/modules/service/infoStore.ts
+++ src/store/modules/service/infoStore.ts
@@ -4,13 +4,13 @@ const state = {
       {
         id: 1,
         title: "Vue.js",
-        version: "3.4.21",
+        version: "3.5.4",
         url: "https://v3.ru.vuejs.org"
       },
       {
         id: 2,
         title: "Vue Router",
-        version: "4.3.0",
+        version: "4.4.4",
         url: "https://router.vuejs.org"
       },
       {
@@ -21,57 +21,93 @@ const state = {
       },
       {
         id: 4,
-        title: "i18n",
+        title: "Webpack",
+        version: "5.5.0",
+        url: "https://webpack.js.org"
+      },
+      {
+        id: 5,
+        title: "Vue I18n",
         version: "9.8.0",
         url: "https://vue-i18n.intlify.dev"
       },
       {
-        id: 5,
+        id: 6,
         title: "HTML5",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/HTML/Introduction_to_HTML"
       },
       {
-        id: 6,
+        id: 7,
         title: "CSS3",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/First_steps"
       },
       {
-        id: 7,
+        id: 8,
         title: "FlexBox CSS",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox"
       },
       {
-        id: 8,
+        id: 9,
         title: "JavaScript",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/JavaScript"
       },
       {
-        id: 9,
+        id: 10,
         title: "TypeScript",
         version: "4.5.5",
         url: "https://www.typescriptlang.org/"
       },
       {
-        id: 10,
+        id: 11,
+        title: "Markdown",
+        version: "",
+        url: "https://www.markdownguide.org"
+      },
+      {
+        id: 12,
         title: "Axios",
-        version: "1.6.2",
+        version: "1.7.9",
         url: "https://axios-http.com"
       },
       {
-        id: 11,
+        id: 13,
         title: "Google Search Console",
         version: "",
         url: "https://search.google.com/search-console"
       },
       {
-        id: 12,
+        id: 14,
         title: "Google Analytics",
         version: "4.0",
         url: "https://analytics.google.com"
+      },
+      {
+        id: 15,
+        title: "Open Graph protocol",
+        version: "",
+        url: "https://ogp.me"
+      },
+      {
+        id: 16,
+        title: "Favicon",
+        version: "",
+        url: "https://developer.mozilla.org/en-US/docs/Glossary/Favicon"
+      },
+      {
+        id: 17,
+        title: "Sitemap",
+        version: "",
+        url: "https://www.sitemaps.org"
+      },
+      {
+        id: 18,
+        title: "Robots.txt",
+        version: "",
+        url: "https://developers.google.com/search/docs/crawling-indexing/robots/intro"
       },
     ]
 };
